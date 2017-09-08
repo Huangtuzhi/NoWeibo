9d0f5e4bbfe59dc5bf3ad4b38d701c6efa5b2f80
huangtuzhi <heying1991@gmail.com>
2017-09-08 12:43:03 +0800

    add logicwq

---
+++ src/noweibo/examples/test.sh
@@ -0,0 +1,1 @@
+go run weibo_demo.go  --access_token=2.003h3j3BD3Yu4R7930636c88axuImiC
--- src/noweibo/examples/weibo_demo.go
+++ src/noweibo/examples/weibo_demo.go
@@ -4,15 +4,43 @@ import (
 	"fmt"
 	"noweibo"
 	"flag"
+	"bufio"
+	"os"
+	"time"
 )
 
 var (
 	access_token = flag.String("access_token", "", "用户的访问令牌")
 )
 
+func CatchCommand(ch chan string) {
+	reader := bufio.NewReader(os.Stdin)
+	data, _, _ := reader.ReadLine()
+	command := string(data)
+	ch <- command
+}
+
+func GetWeibo(ch chan string) {
+	var command string
+	command = <-ch
+	if (command == "start") {
+		var statuses noweibo.Statuses
+		flag.Parse()
+		weibo := noweibo.Weibo{}
+		weibo.CallNet("statuses/home_timeline", "GET", *access_token, &statuses)
+
+		for _, status := range statuses.Statuses {
+			fmt.Println("----------")
+			fmt.Println(status.User.Screen_Name)
+			fmt.Println(status.Text)
+			fmt.Println("\n")
+		}
+	}
+}
+
 func main() {
-	flag.Parse()
-	weibo := noweibo.Weibo{}
-	iRet := weibo.CallNet("statuses/user_timeline", "GET", *access_token)
-	fmt.Println(iRet)
+	ch := make(chan string)
+	go CatchCommand(ch)
+	go GetWeibo(ch)
+	time.Sleep(1e15)
 }
